be458c95b0a9d83ba0c1b8edd88ce8ad4de56063
Darren Cadwallader <darren@bitcrowd.net>
2021-06-24 10:16:03 +0200

    Tweaks padding & color of output error list

--- lib/bitstyles_phoenix/components/error.ex
+++ lib/bitstyles_phoenix/components/error.ex
@@ -21,7 +21,7 @@ defmodule BitstylesPhoenix.Error do
 
   story("Multiple errors", """
       iex> safe_to_string ui_errors(@error_form, :multiple)
-      ~s(<ul>
+      ~s(<ul class="u-padding-l-left u-fg--warning">
         <li><span class=\"u-fg--warning\" phx-feedback-for=\"user_multiple\">is simply bad</span></li>
         <li><span class=\"u-fg--warning\" phx-feedback-for=\"user_multiple\">not fun</span></li>
       </ul>
@@ -40,7 +40,7 @@ defmodule BitstylesPhoenix.Error do
 
       errors ->
         ~E"""
-        <ul><%= for error <- errors do %>
+        <ul class="u-padding-l-left u-fg--warning"><%= for error <- errors do %>
           <li><%= ui_error(form, field, error) %></li><% end %>
         </ul>
         """
