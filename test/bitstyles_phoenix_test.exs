9232aefb77b19435512a4d8fb759ef97340a284d
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2021-11-02 19:42:19 +0100

    Fixed tests

--- test/bitstyles_phoenix_test.exs
+++ test/bitstyles_phoenix_test.exs
@@ -4,7 +4,7 @@ defmodule BitstylesPhoenixTest do
 
   describe "short-cut imports" do
     test "can use the ui_* helpers" do
-      ui_error_tag("foo")
+      ui_button("foo", [])
 
       Phoenix.LiveView.Helpers.component(&ui_badge/1, inner_content: "foo")
     end
