24b90dd7b15d9c411e68168e9a5ba08e6c682572
Andreas Knöpfle <andi@bitcrowd.net>
2021-11-17 09:30:13 +0100

    Remove scrollbars in examples

--- lib/bitstyles_phoenix/showcase.ex
+++ lib/bitstyles_phoenix/showcase.ex
@@ -56,7 +56,7 @@ defmodule BitstylesPhoenix.Showcase do
             ~s(<html style="background-color: transparent;"><head><style>@media (prefers-color-scheme: dark\){body{color: #fff;}}</style><link rel="stylesheet" href="#{dist}/build/bitstyles.css"></head><body>#{Enum.join([extra_html, result]) |> String.replace("\n", "")}</body></html>),
           # https://stackoverflow.com/questions/819416/adjust-width-and-height-of-iframe-to-fit-with-content-in-it
           onload:
-            "javascript:(function(o){o.style.height=(o.contentWindow.document.body.scrollHeight + 5)+\"px\";}(this));",
+            "javascript:(function(o){o.style.height=(o.contentWindow.document.body.scrollHeight + 25)+\"px\";}(this));",
           style: "height:1px;width:100%;border:none;overflow:hidden;margin-left: 1em",
           allowtransparency: "true"
         )
