86a5f815916bb430cbc3715ce32d3398dadba249
Andreas Knöpfle <andi@bitcrowd.net>
2021-11-30 08:56:16 +0100

    [#59] Migrate to bitstyles v3  (#63)
    * [#59] Migrate to bitstyles v3
    
    https://github.com/bitcrowd/bitstyles_phoenix/issues/59
    
    * Migrate version and fix multiple errors output

--- test/bitstyles_phoenix/bitstyles_test.exs
+++ test/bitstyles_phoenix/bitstyles_test.exs
@@ -4,11 +4,22 @@ defmodule BitstylesPhoenix.BitstylesTest do
   import ExUnit.CaptureIO
 
   describe "classname/2" do
-    test "version 3.0.0" do
+    test "version 4.0.0" do
       assert Regex.match?(
-               ~r/3.0.0 of bitstyles is not yet supported/,
-               capture_io(:stderr, fn -> classname("u-flex", "3.0.0") end)
+               ~r/4.0.0 of bitstyles is not yet supported/,
+               capture_io(:stderr, fn -> classname("u-flex", "4.0.0") end)
              )
+    end
+
+    test "version 3.0.0" do
+      assert classname("u-flex", "3.0.0") == "u-flex"
+      assert classname("u-flex-shrink-0", "3.0.0") == "u-flex-shrink-0"
+      assert classname("u-flex-grow-1", "3.0.0") == "u-flex-grow-1"
+      assert classname("u-flex-wrap", "3.0.0") == "u-flex-wrap"
+      assert classname("u-flex-col", "3.0.0") == "u-flex-col"
+      assert classname("u-grid-cols-3", "3.0.0") == "u-grid-cols-3"
+      assert classname("u-col-span-3", "3.0.0") == "u-col-span-3"
+      assert classname("u-col-start-1", "3.0.0") == "u-col-start-1"
     end
 
     test "version 2.0.0" do
