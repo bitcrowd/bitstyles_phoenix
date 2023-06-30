43a7ca574d425c8b6237a5bf7449e06cbe7d3c94
Sabine Holler <sabine@bitcrowd.net>
2023-06-30 15:14:27 +0200

    format file

--- lib/bitstyles_phoenix/component/nav.ex
+++ lib/bitstyles_phoenix/component/nav.ex
@@ -31,101 +31,108 @@ defmodule BitstylesPhoenix.Component.Nav do
       """
   ''')
 
-
-story(
-  "Navbar with list of buttons",
-  '''
-    iex> assigns = %{}
-    ...> render ~H"""
-    ...> <.ui_nav(logo_url: "https://placekitten.com/100/100")>
-    ...>  <:inner_block>
-    ...>     <div class="u-hidden u-block@l u-flex-shrink-1 u-overflow-x-auto">
-    ...>      <ul class="u-flex u-list-none">
-    ...>        <li class="u-margin-m-right">
-    ...>          <a href="/" class="a-button a-button--transparent" aria-current="page">Team</a>
-    ...>        </li>
-    ...>        <li class="u-margin-m-right">
-    ...>          <a href="/" class="a-button a-button--transparent">Projects</a>
-    ...>        </li>
-    ...>        <li class="u-margin-m-right">
-    ...>          <a href="/" class="a-button a-button--transparent">Dashboard</a>
-    ...>        </li>
-    ...>      </ul>
-    ...>     </div>
-    ...>   </:inner_block>
-    ...> </.ui_nav>
-    ...> """
-    """
-    <div class="u-bg-gray-80 u-width-full u-padding-s2-top u-padding-s2-bottom u-relative">
-      <div class="u-padding-m-x u-flex u-justify-between u-items-center u-flex-wrap">
-        <div class="u-flex-shrink-1 u-flex u-items-center">
-          <img src="https://placekitten.com/150/50" width="150" height="50" alt="Company logo" class="u-flex-shrink-0 u-margin-l2-right u-hidden u-block@l" />
-          <img src="https://placekitten.com/50/50" width="50" height="50" alt="Company logo" class="u-flex-shrink-0 u-margin-l2-right u-hidden@l" />
+  story(
+    "Navbar with list of buttons",
+    '''
+        iex> assigns = %{}
+        ...> render ~H"""
+        ...> <.ui_nav(logo_url: "https://placekitten.com/100/100")>
+        ...>  <:inner_block>
+        ...>     <div class="u-hidden u-block@l u-flex-shrink-1 u-overflow-x-auto">
+        ...>      <ul class="u-flex u-list-none">
+        ...>        <li class="u-margin-m-right">
+        ...>          <a href="/" class="a-button a-button--transparent" aria-current="page">Team</a>
+        ...>        </li>
+        ...>        <li class="u-margin-m-right">
+        ...>          <a href="/" class="a-button a-button--transparent">Projects</a>
+        ...>        </li>
+        ...>        <li class="u-margin-m-right">
+        ...>          <a href="/" class="a-button a-button--transparent">Dashboard</a>
+        ...>        </li>
+        ...>      </ul>
+        ...>     </div>
+        ...>   </:inner_block>
+        ...> </.ui_nav>
+        ...> """
+        """
+        <div class="u-bg-gray-80 u-width-full u-padding-s2-top u-padding-s2-bottom u-relative">
+          <div class="u-padding-m-x u-flex u-justify-between u-items-center u-flex-wrap">
+            <div class="u-flex-shrink-1 u-flex u-items-center">
+              <img src="https://placekitten.com/150/50" width="150" height="50" alt="Company logo" class="u-flex-shrink-0 u-margin-l2-right u-hidden u-block@l" />
+              <img src="https://placekitten.com/50/50" width="50" height="50" alt="Company logo" class="u-flex-shrink-0 u-margin-l2-right u-hidden@l" />
+            </div>
+            <div class="u-flex-shrink-1 u-overflow-x-auto">
+              <ul class="u-flex u-list-none">
+                <li class="u-margin-m-right">
+                  <a href="/" class="a-button a-button--transparent" aria-current="page">Team</a>
+                </li>
+                <li class="u-margin-m-right">
+                  <a href="/" class="a-button a-button--transparent">Projects</a>
+                </li>
+                <li class="u-margin-m-right">
+                  <a href="/" class="a-button a-button--transparent">Dashboard</a>
+                </li>
+              </ul>
+            </div>
+            </div>
+          </div>
         </div>
-        <div class="u-flex-shrink-1 u-overflow-x-auto">
-          <ul class="u-flex u-list-none">
-            <li class="u-margin-m-right">
-              <a href="/" class="a-button a-button--transparent" aria-current="page">Team</a>
-            </li>
-            <li class="u-margin-m-right">
-              <a href="/" class="a-button a-button--transparent">Projects</a>
-            </li>
-            <li class="u-margin-m-right">
-              <a href="/" class="a-button a-button--transparent">Dashboard</a>
-            </li>
-          </ul>
-        </div>
-        </div>
-      </div>
-    </div>
-    """
-''')
+        """
+    '''
+  )
 
   story(
     "Navbar with left block",
     '''
-      iex> assigns = %{}
-      ...> render ~H"""
-      ...> <.ui_nav(logo_url: "https://placekitten.com/100/100")>
-      ...>  <:left_block>
-      ...>     Content
-      ...>   </:left_block>
-      ...> </.ui_nav>
-      ...> """
-      """
-      <div class="u-bg-gray-80 u-width-full u-padding-s2-top u-padding-s2-bottom u-relative">
-        <div class="u-padding-m-x u-flex u-justify-between u-items-center u-flex-wrap">
-          <div class="u-flex-shrink-1 u-flex u-items-center">
-            <img src="https://placekitten.com/150/50" width="150" height="50" alt="Company logo" class="u-flex-shrink-0 u-margin-l2-right u-hidden u-block@l" />
-            <img src="https://placekitten.com/50/50" width="50" height="50" alt="Company logo" class="u-flex-shrink-0 u-margin-l2-right u-hidden@l" />
+          iex> assigns = %{}
+          ...> render ~H"""
+          ...> <.ui_nav(logo_url: "https://placekitten.com/100/100")>
+          ...>  <:left_block>
+          ...>     Content
+          ...>   </:left_block>
+          ...> </.ui_nav>
+          ...> """
+          """
+          <div class="u-bg-gray-80 u-width-full u-padding-s2-top u-padding-s2-bottom u-relative">
+            <div class="u-padding-m-x u-flex u-justify-between u-items-center u-flex-wrap">
+              <div class="u-flex-shrink-1 u-flex u-items-center">
+                <img src="https://placekitten.com/150/50" width="150" height="50" alt="Company logo" class="u-flex-shrink-0 u-margin-l2-right u-hidden u-block@l" />
+                <img src="https://placekitten.com/50/50" width="50" height="50" alt="Company logo" class="u-flex-shrink-0 u-margin-l2-right u-hidden@l" />
+              </div>
+              <div class="u-fg-white u-margin-s-left items-end">
+                <p class="">Left Block</p>
+              </div>
+            </div>
           </div>
-          <div class="u-fg-white u-margin-s-left items-end">
-            <p class="">Left Block</p>
-          </div>
-        </div>
-      </div>
-      """
-''')
-
-
+          """
+    '''
+  )
 
   def ui_nav(assigns) do
-
-    class= classnames([
-      "u-bg-gray-80 u-padding-s2-top u-padding-s2-bottom u-width-full u-relative",
-      assigns[:class]
-    ])
+    class =
+      classnames([
+        "u-bg-gray-80 u-padding-s2-top u-padding-s2-bottom u-width-full u-relative",
+        assigns[:class]
+      ])
 
     inner_block_class =
       classnames([
         "u-margin-s-left",
         assigns[:inner_block_class]
       ])
-      left_block_class =
+
+    left_block_class =
       classnames([
         "u-margin-s-left items-end",
         assigns[:left_block_class]
       ])
+
+    assigns =
+      assign(assigns,
+        class: class,
+        inner_block_class: inner_block_class,
+        left_block_class: left_block_class
+      )
 
     ~H"""
     <nav class={@class}>
